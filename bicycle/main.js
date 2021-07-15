e11cc11fc85810504f6bc975a4dd84cd49cca229
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-07-15 23:24:59 +0900

    a

--- bicycle/main.js
+++ bicycle/main.js
@@ -11,14 +11,8 @@ const ctx = canvas.getContext("2d");
 const canvasWidth = 720;
 const canvasHeight = 480;
 
-const courseDiffTotalCount = 100 * 100;
-const courseDiffCountToFillCanvas = 200;
-const courseDiffWidth = canvasWidth / courseDiffCountToFillCanvas;
-
-/**
- * @type {Array<{ h: number }>}
- */
-const courseDiffs = [];
+const courseBlockCountToFillCanvas = 100;
+const courseBlockWidth = canvasWidth / courseBlockCountToFillCanvas;
 
 function setCanvasSize() {
   canvasWrapper.style.width = `${canvasWidth}px`;
@@ -26,49 +20,65 @@ function setCanvasSize() {
   canvas.height = canvasHeight;
 }
 
-function fillRectOnGround(x, w, h) {
+function drawRect(x, w, h) {
   const y = canvasHeight - h;
   ctx.fillRect(x, y, w, h);
 }
 
-function setCourse() {
-  let h = 100;
-  let prevHDiff = 0;
-  for (let i = 0; i < courseDiffTotalCount; i++) {
-    const hDiff = [-5, -5, -5, 0, 5, 5, 5][randomInt(0, 6)];
-    if (h + hDiff > canvasHeight / 10 && h + hDiff < canvasHeight / 2 && prevHDiff * hDiff >= 0) {
-      h += hDiff;
-      prevHDiff = hDiff;
-    }
-    courseDiffs.push({ h });
-  }
+function drawPlayer(x, y, radius) {
+  const circle = new Path2D();
+  circle.arc(x, canvasHeight - y, radius, 0, 2 * Math.PI);
+  const prevFillStyle = ctx.fillStyle;
+  ctx.fillStyle = "blue";
+  ctx.fill(circle);
+  ctx.fillStyle = prevFillStyle;
 }
 
-setCanvasSize();
-setCourse();
+function createCourseBlocks() {
+  const maxHeight = canvasHeight * 0.7;
+  const minHeight = canvasHeight * 0.1;
+  const diffHeight = 5;
+  const laps = 10;
 
-let i = 0;
-setInterval(function () {
-  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-  for (let j = 0; j < courseDiffCountToFillCanvas; j++) {
-    const idx = (j + i) % courseDiffs.length;
-    const { h } = courseDiffs[idx];
-    fillRectOnGround(courseDiffWidth * j, courseDiffWidth, h);
+  let h = minHeight + diffHeight * 10;
+  let dh = [-diffHeight, 0, diffHeight][randomInt(0, 2)];
+  const res = [h + dh];
+
+  for (let i = 1; i < laps * courseBlockCountToFillCanvas; i++) {
+    if (h > maxHeight || h < minHeight) {
+      dh = h > maxHeight ? -diffHeight : diffHeight;
+      h += dh;
+      res.push(h);
+      continue;
+    }
+
+    if (dh === 0) {
+      dh = [-diffHeight, 0, diffHeight][randomInt(0, 2)];
+    } else {
+      dh = [dh, dh, dh, dh, 0][randomInt(0, 4)];
+    }
+    h += dh;
+    res.push(h);
   }
-  drawPlayer(0, canvasHeight - courseDiffs[i].h - 10, 10);
-  i++;
-}, 50);
+
+  return res;
+}
 
 function randomInt(min, max) {
   const size = max - min + 1;
   return Math.floor(Math.random() * size) + min;
 }
 
-function drawPlayer(x, y, size) {
-  const circle = new Path2D();
-  circle.arc(x, y, size, 0, 2 * Math.PI);
-  const fillStyle = ctx.fillStyle;
-  ctx.fillStyle = "blue";
-  ctx.fill(circle);
-  ctx.fillStyle = fillStyle;
-}
+setCanvasSize();
+
+const courseBlocks = createCourseBlocks();
+let currentPositionIndex = 0;
+
+setInterval(function () {
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+  for (let i = 0; i < courseBlockCountToFillCanvas; i++) {
+    const courseIndex = (currentPositionIndex + i) % courseBlocks.length;
+    drawRect(courseBlockWidth * i, courseBlockWidth, courseBlocks[courseIndex]);
+  }
+  currentPositionIndex++;
+}, 50);
