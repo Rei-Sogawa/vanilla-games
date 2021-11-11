760f3b3a468a22f343ecc9a0ceac8aaa79814136
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-11-11 22:00:55 +0900

    deploy

---
+++ public/air-hocker/main.js
@@ -0,0 +1,223 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+
+var paddleHeight = 10;
+var paddleWidth = 75;
+
+var playerPaddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+var topPressed = false;
+
+var cpuPaddleX = (canvas.width - paddleWidth) / 2;
+var noiseToCpuPaddleX = 0;
+
+var cutSound = new Audio("./air-hocker/cut.mp3");
+var driveSound = new Audio("./air-hocker/drive.mp3");
+
+var playerLives = 3;
+var cpuLives = 3;
+
+var cutEffectSizeList = [-1, 0, 0, 1];
+var driveEffectSizeList = [0, 0, 1];
+
+var canDraw = true;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = true;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = true;
+  } else if (e.key == "Up" || e.key == "ArrowUp") {
+    topPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if (e.key == "Right" || e.key == "ArrowRight") {
+    rightPressed = false;
+  } else if (e.key == "Left" || e.key == "ArrowLeft") {
+    leftPressed = false;
+  } else if (e.key == "Up" || e.key == "ArrowUp") {
+    topPressed = false;
+  }
+}
+
+function updateCpuPaddleX() {
+  cpuPaddleX = x - paddleWidth / 2;
+  noiseToCpuPaddleX += [0.025, -0.025][randomInt(2)];
+  cpuPaddleX += (paddleWidth / 2) * noiseToCpuPaddleX;
+  if (cpuPaddleX <= 0) cpuPaddleX = 0;
+  if (cpuPaddleX + paddleWidth >= canvas.width)
+    cpuPaddleX = canvas.width - paddleWidth;
+}
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "orange";
+  ctx.fill();
+  ctx.closePath();
+}
+function drawPlayerPaddle() {
+  ctx.beginPath();
+  ctx.rect(
+    playerPaddleX,
+    canvas.height - paddleHeight,
+    paddleWidth,
+    paddleHeight
+  );
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+function drawCpuPaddle() {
+  updateCpuPaddleX();
+  ctx.beginPath();
+  ctx.rect(cpuPaddleX, 0, paddleWidth, paddleHeight);
+  ctx.fillStyle = "magenta";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawLives() {
+  ctx.font = "16px monospace";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Player Lives: " + playerLives, canvas.width - 150, 20);
+  ctx.fillText("Cpu Lives:    " + cpuLives, canvas.width - 150, 40);
+}
+
+function drawBallSpeed() {
+  ctx.font = "16px monospace";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("|dx|: " + Math.abs(dx), canvas.width - 150, 80);
+  ctx.fillText("|dy|: " + Math.abs(dy), canvas.width - 150, 100);
+}
+
+function draw() {
+  if (!canDraw) return;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  drawPlayerPaddle();
+  drawCpuPaddle();
+  drawLives();
+  drawBallSpeed();
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if (y + dy < ballRadius) {
+    if (
+      x + ballRadius > cpuPaddleX &&
+      x - ballRadius < cpuPaddleX + paddleWidth
+    ) {
+      dy = -dy;
+
+      // cpu のカット・ドライブの処理
+      var cut = cutEffectSizeList[randomInt(cutEffectSizeList.length)];
+      var drive = driveEffectSizeList[randomInt(driveEffectSizeList.length)];
+      dx += cut;
+      dy += drive;
+
+      // cpu のカット・ドライブ時の効果音
+      if (cut !== 0) {
+        cutSound.play();
+      }
+      if (drive !== 0) {
+        driveSound.play();
+      }
+    } else {
+      cpuLives--;
+      if (cpuLives === 0) {
+        alert("YOU WIN!");
+        document.location.reload();
+        clearInterval(interval);
+      } else {
+        canDraw = false;
+        setTimeout(() => {
+          x = canvas.width / 2;
+          y = 30;
+          dx = 2;
+          dy = 2;
+          playerPaddleX = (canvas.width - paddleWidth) / 2;
+
+          noiseToCpuPaddleX = 0;
+
+          canDraw = true;
+        }, 1000);
+      }
+    }
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (
+      x + ballRadius > playerPaddleX &&
+      x - ballRadius < playerPaddleX + paddleWidth
+    ) {
+      dy = -dy;
+
+      // カット・ドライブの処理
+      if (rightPressed) {
+        dx += 1;
+      }
+      if (leftPressed) {
+        dx -= 1;
+      }
+      if (topPressed) {
+        dy -= 1;
+      }
+
+      // カット・ドライブの効果音
+      if (rightPressed || leftPressed) {
+        cutSound.play();
+      }
+      if (topPressed) {
+        driveSound.play();
+      }
+    } else {
+      playerLives--;
+      if (playerLives === 0) {
+        alert("GAME OVER");
+        document.location.reload();
+        clearInterval(interval); // Needed for Chrome to end game
+      } else {
+        canDraw = false;
+        setTimeout(() => {
+          x = canvas.width / 2;
+          y = canvas.height - 30;
+          dx = 2;
+          dy = -2;
+          playerPaddleX = (canvas.width - paddleWidth) / 2;
+
+          noiseToCpuPaddleX = 0;
+
+          canDraw = true;
+        }, 1000);
+      }
+    }
+  }
+
+  if (rightPressed && playerPaddleX < canvas.width - paddleWidth) {
+    playerPaddleX += 7;
+  } else if (leftPressed && playerPaddleX > 0) {
+    playerPaddleX -= 7;
+  }
+
+  x += dx;
+  y += dy;
+}
+
+var interval = setInterval(draw, 10);
+
+// utils
+function randomInt(length, startAt = 0) {
+  return Math.floor(Math.random() * length) + startAt;
+}
---
+++ public/mine-sweeper/main.js
@@ -0,0 +1,248 @@
+const board = document.getElementById("board");
+
+let width;
+let height;
+let bombAmount;
+
+let initialized = false;
+let finished = false;
+
+let timer;
+
+const clearAudio = new Audio("./mine-sweeper/clear.mp3");
+const bombAudio = new Audio("./mine-sweeper/bomb.mp3");
+
+function createBoard() {
+  for (let i = 0; i < height; i++) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+
+    board.appendChild(column);
+
+    for (let j = 0; j < width; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square", "hidden");
+      square.id = `${i}-${j}`;
+      column.appendChild(square);
+      square.addEventListener("click", function () {
+        handleClick(i, j);
+      });
+    }
+  }
+}
+
+function setBomb() {
+  const bombArray = getBombArray();
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      const square = document.getElementById(`${i}-${j}`);
+      if (square.classList.contains("blocked")) {
+        square.classList.remove("blocked");
+      } else {
+        if (bombArray.pop() == "bomb") {
+          square.classList.add("bomb");
+        }
+      }
+    }
+  }
+}
+
+function setBombAmount() {
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      const square = document.getElementById(`${i}-${j}`);
+      const bombAmountAround = getBombAmountAround(i, j);
+      if (!square.classList.contains("bomb") && bombAmountAround > 0) {
+        square.classList.add("danger");
+        const text = document.createTextNode(bombAmountAround);
+        square.appendChild(text);
+      }
+    }
+  }
+}
+
+function blockAround(i, j) {
+  for (let neighborI = i - 1; neighborI <= i + 1; neighborI++) {
+    for (let neighborJ = j - 1; neighborJ <= j + 1; neighborJ++) {
+      const square = document.getElementById(`${neighborI}-${neighborJ}`);
+      if (square) {
+        square.classList.add("blocked");
+      }
+    }
+  }
+}
+
+function getBombArray() {
+  const blockAmount = document.getElementsByClassName("blocked").length;
+  const onlyBombArray = new Array(bombAmount).fill("bomb");
+  const onlyEmptyArray = new Array(
+    width * height - bombAmount - blockAmount
+  ).fill("empty");
+  const concattedArray = onlyBombArray.concat(onlyEmptyArray);
+  const bombArray = _.shuffle(concattedArray);
+  return bombArray;
+}
+
+function getBombAmountAround(i, j) {
+  let bombAmountAround = 0;
+  for (let neighborI = i - 1; neighborI <= i + 1; neighborI++) {
+    for (let neighborJ = j - 1; neighborJ <= j + 1; neighborJ++) {
+      const isSelf = i == neighborI && j == neighborJ;
+      const neighborSquare = document.getElementById(
+        `${neighborI}-${neighborJ}`
+      );
+      if (
+        !isSelf &&
+        neighborSquare &&
+        neighborSquare.classList.contains("bomb")
+      ) {
+        bombAmountAround += 1;
+      }
+    }
+  }
+  return bombAmountAround;
+}
+
+function handleClick(i, j) {
+  if (initialized) {
+    click(i, j);
+  } else {
+    firstClick(i, j);
+  }
+}
+
+function click(i, j) {
+  if (finished) {
+    return;
+  }
+
+  const square = document.getElementById(`${i}-${j}`);
+  if (square.classList.contains("bomb")) {
+    window.alert("BOMB!");
+    bombAudio.play();
+    openAll();
+    clearInterval(timer);
+    finished = true;
+    return;
+  }
+  sweep(i, j);
+
+  if (
+    document.getElementsByClassName("hidden").length ==
+    document.getElementsByClassName("bomb").length
+  ) {
+    window.alert("CLEAR!");
+    clearAudio.play();
+    openAll();
+    setTimeout(function () {
+      replaceBombWithFlower();
+    }, 1000);
+    clearInterval(timer);
+    finished = true;
+    return;
+  }
+}
+
+function firstClick(i, j) {
+  blockAround(i, j);
+  setBomb();
+  setBombAmount();
+  sweep(i, j);
+  startTimer();
+  initialized = true;
+}
+
+function sweep(i, j) {
+  const square = document.getElementById(`${i}-${j}`);
+  if (!square || square.classList.contains("open")) {
+    return;
+  }
+  square.classList.remove("hidden");
+  square.classList.add("open");
+  if (square.classList.contains("danger")) {
+    return;
+  }
+  for (let neighborI = i - 1; neighborI <= i + 1; neighborI++) {
+    for (let neighborJ = j - 1; neighborJ <= j + 1; neighborJ++) {
+      const isSelf = i == neighborI && j == neighborJ;
+      const neighborSquare = document.getElementById(
+        `${neighborI}-${neighborJ}`
+      );
+      if (!isSelf && neighborSquare) {
+        sweep(neighborI, neighborJ);
+      }
+    }
+  }
+}
+
+function openAll() {
+  const squares = Array.from(document.getElementsByClassName("square"));
+  for (const square of squares) {
+    square.classList.remove("hidden", "flag");
+    square.classList.add("open");
+  }
+}
+
+function createContextMenu() {
+  $.contextMenu({
+    selector: ".square",
+    callback: function (key, options) {
+      const square = options.$trigger[0];
+      if (key == "addFlag") {
+        square.classList.add("flag");
+      } else if (key == "removeFlag") {
+        square.classList.remove("flag");
+      }
+    },
+    items: {
+      addFlag: { name: "フラグを立てる" },
+      removeFlag: { name: "フラグを回収する" },
+    },
+  });
+}
+
+function startTimer() {
+  const startTimestamp = new Date().getTime();
+  timer = setInterval(function () {
+    const currentTimestamp = new Date().getTime();
+    const time = Math.floor((currentTimestamp - startTimestamp) / 1000);
+    document.getElementById("timer").innerText = `${time} s`;
+  }, 1000);
+}
+
+function replaceBombWithFlower() {
+  const bombSquares = Array.from(document.getElementsByClassName("bomb"));
+  for (const square of bombSquares) {
+    square.classList.remove("bomb");
+    square.classList.add("flower");
+  }
+}
+
+document.getElementById("select-level-buttons").addEventListener(
+  "click",
+  function (e) {
+    const level = e.target.id;
+    switch (level) {
+      case "beginner": {
+        width = 9;
+        height = 9;
+        bombAmount = 10;
+        break;
+      }
+      case "intermediate": {
+        width = 16;
+        height = 16;
+        bombAmount = 40;
+        break;
+      }
+      case "advanced": {
+        width = 30;
+        height = 16;
+        bombAmount = 99;
+      }
+    }
+    createBoard();
+    createContextMenu();
+  },
+  { once: true }
+);
